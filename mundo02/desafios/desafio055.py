c32134fc8a26409960280bb8a7cec70436b13d24
Rafael <93839545+Rafa2697@users.noreply.github.com>
2023-07-18 14:30:28 -0300

    update

--- mundo02/desafios/desafio055.py
+++ mundo02/desafios/desafio055.py
@@ -1,1 +1,16 @@
 #faça um porgrama que leia o peso de 5 pessoas e no final, mostre qual foi o maior e o menor peso lidos.
+
+maior = 0
+menor = 0
+for p in range (1, 6):
+    peso = float(input('informe o {}º peso: '.format(p)))
+    if p == 1:
+        maior = peso
+        menor = peso
+    else:
+        if peso > maior:
+            maior = peso
+        if peso < menor:
+            menor = peso
+print('O maior peso lido foi de {}KG'.format(maior))
+print('O menor peso lido foi de {}KG'.format(menor))
--- mundo02/desafios/desafio056.py
+++ mundo02/desafios/desafio056.py
@@ -1,5 +1,28 @@
-#desenvolva um programa que leia nome, idade, e sexo de 4 oessias. NO final do programa, mostre:
+#desenvolva um programa que leia nome, idade, e sexo de 4 pessoas. NO final do programa, mostre:
 '''
 A média de idade do grupo.
 qual é o nome do homem mais velho
 quantas mulheres tem menos de 20 anos'''
+somaidade = 0
+mediaidade = 0
+maioridadehomem = 0
+nomevelho = ''
+totmulher20 = 0
+for p in range(1,5):
+    print('---- {}ª PESSOA -----'.format(p))
+    nome = str(input('Nome: ')).strip()
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    somaidade += idade
+    if p == 1 and sexo in 'Mm':
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Mm' and idade > maioridadehomem:
+        maioridadehomem = idade
+        nomevelho = nome
+    if sexo in 'Ff' and idade < 20:
+        totmulher20 = totmulher20 + 1
+mediaidade = somaidade / 4
+print('a media de idade do grupo é de {} anos'.format(mediaidade))
+print('O homem mais velho tem {} anos e se chama {}.'.format(maioridadehomem, nomevelho))
+print('ao todo são {} mulhere(s) com menos de 20 anos.'.format(totmulher20))
