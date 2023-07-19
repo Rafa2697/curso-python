7806b04ce69a02d5829c69306b4c7b674c74b3ac
Rafael <93839545+Rafa2697@users.noreply.github.com>
2023-07-18 23:43:49 -0300

    Update desafio060.py

--- mundo02/desafios/desafio060.py
+++ mundo02/desafios/desafio060.py
@@ -1,3 +1,10 @@
 '''faça um porgrama que leia um numero qualquer e mostre o seu fatorial.
 
 ex 5! = 5x4x3x2x1 = 120'''
+
+numero = int(input('Digite uma numero: '))
+c = numero
+while c > 0:
+    print('{}'.format(c), end=' ')
+    c = c-1
+#print('fatorial desse numero é {}'.format())
