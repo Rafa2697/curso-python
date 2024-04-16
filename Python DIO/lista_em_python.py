bc753e415e89d59d7abfdb73f3b85d280f4534eb
Rafael <Rafael.mattos468@gmail.com>
2024-04-16 18:48:21 -0300

    update

---
+++ Python DIO/lista_em_python.py
@@ -0,0 +1,70 @@
+########## criação e acesso de dados ##############
+
+# carro = ["Marca", "modelo", 4200000, 2020, 2900, "são paulo", True]
+
+# print(carro[0])
+# print(carro[2])
+# print(carro)
+
+# matriz = [
+    
+#     [1,"a",2],
+#     ["b",3,2],
+#     [1,5,"c"]
+# ]
+
+# print(matriz)
+# print(matriz[0])
+# print(matriz[0][1])
+# print(matriz[2][2])
+
+
+# carros = ["gol", "celta", "uno"]
+
+# for i, carro in enumerate(carros):
+#     print(f"{i}: {carro}")
+    
+# numeros = [1, 30, 21, 2, 9, 65, 34]
+# pares = []
+
+# for numero in numeros:
+#     if numero % 2 == 0:
+#         pares.append(numero) #append adiciona o indice na lista pares.
+        
+# print(pares)
+
+
+############# métodos de classe list ###########
+
+# lista = []
+
+# #append
+# lista.append(1)
+# lista.append("rafael")
+# lista.append(True)
+
+# print(lista)
+
+# #clear
+# lista.clear()
+# print(lista)
+
+# #copy
+
+# lista = [1, "python", [40,30,20]]
+
+# lista.copy()
+
+# print(lista)
+
+#count
+cores = ["vermelho", "azul", "verde", "azul"]
+cores.count("vermelho")
+cores.count("Azul")
+cores.count("verde")
+
+print(cores)
+
+cores.extend(["cinza", "marrom"]) #acrescenta uma lista a outra.
+
+print(cores)
--- Python DIO/manipulando_strings.py
+++ Python DIO/manipulando_strings.py
@@ -1,30 +1,55 @@
 ###############Conhecendo métodos úteis da classe string############
-curso = "pYtHoN"
+# curso = "pYtHoN"
 
-print(curso.upper()) #deixa toda a string e maiuscula 
-print(curso.lower()) #deixa toda a string em minuscula
-print(curso.title()) #Deixa somente a primeira letra maiscula e as demais minuscula
+# print(curso.upper()) #deixa toda a string e maiuscula 
+# print(curso.lower()) #deixa toda a string em minuscula
+# print(curso.title()) #Deixa somente a primeira letra maiscula e as demais minuscula
 
-print(curso.strip() + (".")) #remove os espaços da string
-print(curso.lstrip() + ("."))#remove um epaço do lado esquerdo
-print(curso.rstrip() + ("."))#remove um epaço do lado direito
+# print(curso.strip() + (".")) #remove os espaços da string
+# print(curso.lstrip() + ("."))#remove um epaço do lado esquerdo
+# print(curso.rstrip() + ("."))#remove um epaço do lado direito
 
-#Junções e centralização
-print(curso.center(12, '-')) # centraliza a string acrescentando outros caracteres e espaço em branco caso não coloque o caracter.
-print('.'.join(curso)) #separa a string letra por letra, usando o . como separador. 
+# #Junções e centralização
+# print(curso.center(12, '-')) # centraliza a string acrescentando outros caracteres e espaço em branco caso não coloque o caracter.
+# print('.'.join(curso)) #separa a string letra por letra, usando o . como separador. 
 
 
-################ interpolação de variáveis ##################
+# ################ interpolação de variáveis ##################
 
-nome = "rafael"
-idade = 26
-profissao = "programador"
-linguagem = "Python"
+# nome = "rafael"
+# idade = 26
+# profissao = "programador"
+# linguagem = "Python"
 
-print(f"Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matrículado no curso de {linguagem}")
+# dados = {"nome": "Guilherme", "idade": 28}
 
-PI = 3.14159
+# print(f"Olá, me chamo {nome}. Eu tenho {idade} anos de idade, trabalho como {profissao} e estou matrículado no curso de {linguagem}")
 
-print(f"Valor de PI: {PI: .2f}")
+# PI = 3.14159
+
+# print(f"Valor de PI: {PI: .2f}")
+
+# print("nome: {nome} Idade: {idade}".format(**dados))
+
+
+############## fatiamento de string #################
+
+nome = "python"
+
+# print(nome[:6])
+# print(nome[10:])
+# print(nome[7:10])
+# print(nome[:])
+print(nome[:: -1])
+
+
+################# String Múltiplas linhas ####################
+
+# nome = "rafael"
+
+# mensagem = f'''Olá meu nome é {nome}, Estou aprendendo python. 
+#     essa mensagem tem diferentes recuos'''
+    
+# print(mensagem)
 
 
